bab9ae27d7006037891774d3dec74e3060395ee1
Cindy Cristina Yañez <cindyyanez97@gmail.com>
2021-10-31 10:04:33 -0700

    Produced a quality control report
    Added a section to output a quality control report that includes basic stats and some data red flags

--- limeavocado_picarro_G2401.R
+++ limeavocado_picarro_G2401.R
@@ -1,18 +1,18 @@
 ## Script for processing of Picarro G2401 data 
 ## Author: Cindy Yanez
-## Last revised: Oct. 21, 2021
+## Last revised: Oct. 31, 2021
 # See README.txt file for definition of data levels
 
 # User inputs -------------------------------------------------------------
-routeID <- "20190715" # enter routeID for survey
-main <- '/Volumes/Seagate Expansion Drive/ucrlimeavocado/Data/' # main path to data, all other paths are relative to this one
+routeID <- "20190731" # enter routeID for survey
+main <- '/Volumes/cindrive/ucrlimeavocado/Data/' # main path to data, all other paths are relative to this one
 logFile <- paste(main, 'logs2.xlsx', sep='') # path to log file with metadata
 raw_file_path <- paste(main, 'raw/Picarro G2401/', sep = '') # input path to raw Picarro files
 v1_file_path <- paste(main, 'processed/Level01/Picarro G2401/', sep='') # output path for Level 1 files
 v2_file_path <- paste(main, 'processed/Level02/Picarro G2401/', sep='') # output path for Level 2 files
 calSum_file_path <- paste(main, 'processed/calibration_summary/', sep='') # output path for calibration summary files
 v3_file_path <- paste(main, 'processed/Level03/Picarro G2401/', sep='') # output path for Level 3 files
-qaqc_file_path <- paste(main,'QAQC/Picarro G2401/',sep='') # output path for quality control report
+report_file_path <- paste(main,'reports/Picarro G2401/',sep='') # output path for processing report
 
 # Libraries ---------------------------------------------------------------
 library(data.table)
@@ -22,21 +22,23 @@ library(stringr)
 library(dplyr)
 library(readxl)
 library(purrr)
-source('~/UCR LIME AVOCADO/limeavocado/limeavocado_functions.R')
-source('~/UCR LIME AVOCADO/limeavocado/get_calib_values.R')
-source('~/UCR LIME AVOCADO/limeavocado/calibrate.R')
+source('~/limeavocado/limeavocado_functions.R')
+source('~/limeavocado/get_calib_values.R')
+source('~/limeavocado/calibrate.R')
 
 # Level 1 Processing ------------------------------------------------------------
 surveyInfo = read_excel(logFile, sheet = 'GENERAL') %>% filter(ID == routeID) # import general survey information from log file
 files = get_files(surveyInfo, in_path = raw_file_path)  # Finds the data files in your computer using survey info
 picarro_v1 = create_level_1(files, v1_file_path) # Level 1 data gets exported
+print("Level 1 data has been created and exported")
 
 # Level 2 Processing ------------------------------------------------------------
 offset <- read_excel(logFile, sheet = 'TIMEOFFSET') %>% filter(ID == routeID) # import Picarro G2401 time offset
 picarro_v2 <- create_level_2(picarro_v1, offset$G2401, v2_file_path) # Level 2 data gets exported
+print("Level 2 data has been created and exported")
 
 # Level 3 Processing  ---------------------------------------------
-picarro_gps_weather <- merge_picarro_datalogger(picarro_v2, surveyInfo$GPSFILE, surveyInfo$WEATHERFILE) %>% # merge Level 2 data with gps and weather
+picarro_gps_weather <- merge_picarro_datalogger(picarro_v2, paste(main, surveyInfo$GPSFILE,sep=""), paste(main,surveyInfo$WEATHERFILE,sep="")) %>% # merge Level 2 data with gps and weather
   select(-c(CH4, CO2)) %>% rename(CH4 = CH4_dry, CO2 = CO2_dry) # from here on, only use the CO2_dry and CH4_dry columns, they are renamed to CO2 and CH4
 
 # CALIBRATE ---------------------------------------------------------------
@@ -49,11 +51,6 @@ measured <- get_calib_values(calInfo)
 standards <- read_excel(logFile, sheet = 'STANDARDS') %>% filter(STANDARD_ID %in% calInfo$STANDARD_ID) 
 standards <- standards[order(standards$STANDARD_ID),]
 
-# plot the standard vs measured values
-par(mfrow=c(1,3))
-plot(standards$CO2, measured$CO2)
-plot(standards$CO, measured$CO)
-plot(standards$CH4, measured$CH4)
 
 cal <- calibrate(measured, standards) # calculate coefficients for correction
   
@@ -81,11 +78,12 @@ write.csv(picarro_v3, file=paste(v3_file_path, "picarro-G2401-",routeID,"_v3.csv
 print("Calibrated data has been saved as a csv.")
 
 # Quality Control Report --------------------------------------------------
+
 qc <- list("Route ID" = routeID,
            "Files found" = length(files),
-           "Level 1 length" = length(picarro_v1),
-           "Level 2 length" = length(picarro_v2),
-           "Level 3 length" = length(picarro_v3),
+           "Level 1 length" = nrow(picarro_v1),
+           "Level 2 length" = nrow(picarro_v2),
+           "Level 3 length" = nrow(picarro_v3),
            "Missing values" = sum(is.na(picarro_v3)),
            "Negative CO2 values" = sum(picarro_v3$CO2 < 0),
            "Negative CO values" = sum(picarro_v3$CO < 0),
@@ -95,15 +93,33 @@ qc <- list("Route ID" = routeID,
            "Minimum CH4 (ppm)" = min(picarro_v3$CH4),
            "Maximum CO2 (ppm)" = max(picarro_v3$CO2),
            "Maximum CO (ppb)" = max(picarro_v3$CO),
-           "Maximum CH4 (ppm)" = max(picarro_v3$CH4)
-)
-qc
+           "Maximum CH4 (ppm)" = max(picarro_v3$CH4),
+           "CO2 cal type" = paste(cal["CO2","points"], "point calibration", sep =" "),
+           "CO cal type" = paste(cal["CO","points"], "point calibration", sep =" "),
+           "CH4 cal type" = paste(cal["CH4","points"], "point calibration", sep =" "),
+           "CO2 intercept" = cal["CO2","intercept"],
+           "CO intercept" = cal["CO","intercept"],
+           "CH4 intercept" = cal["CH4","intercept"],
+           "CO2 slope" = cal["CO2","slope"],
+           "CO slope" = cal["CO","slope"],
+           "CH4 slope" = cal["CH4","slope"])
 
-par(mfrow=c(3,1))
+print(as.matrix(qc))
+
+# plot the standard vs measured values
+par(mfrow=c(1,3))
+plot(standards$CO2, measured$CO2, pch = 19, cex = 1.5)
+plot(standards$CO, measured$CO, pch = 19, cex = 1.5)
+plot(standards$CH4, measured$CH4, pch = 19, cex = 1.5)
+
+# # plot timeseries for each gas
+par(mfrow=c(1,1))
 plot(picarro_v3$TIMESTAMP, picarro_v3$CO2)
 plot(picarro_v3$TIMESTAMP, picarro_v3$CO)
 plot(picarro_v3$TIMESTAMP, picarro_v3$CH4)
 
-# Future to do list -------------------------------------------------------
-# output calibration summary statistics
+# produce a report
+# rmarkdown::render(input = "~/limeavocado/limeavocado_picarro_G2401.R",
+#                   output_format = "pdf_document",
+#                   output_file = paste(report_file_path,"picarro_G2401_processing_report_",routeID,".pdf", sep=""))
 
